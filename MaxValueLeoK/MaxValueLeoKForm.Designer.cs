e2bb235d481c7a14f7a0c55d7a7a768901c97abf
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-05-07 12:52:55 -0400

    Initial commit

---
+++ MaxValueLeoK/MaxValueLeoKForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MaxValueLeoK
+{
+    partial class frmMaxValueLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(98, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 33);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(49, 51);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(174, 160);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(45, 228);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 24);
+            this.lblAnswer.TabIndex = 2;
+            // 
+            // frmMaxValueLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMaxValueLeoK";
+            this.Text = "Finding Max Value by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ MaxValueLeoK/MaxValueLeoKForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxValueLeoK
+{
+    public partial class frmMaxValueLeoK : Form
+    {
+        public frmMaxValueLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private int GetMaxValue(int[] tmpArrayOfIntegers)
+        {
+            //variables
+            int tmpMaxValue = 0;
+            int tmpCounter;
+
+            for (tmpCounter = 0; tmpCounter < tmpArrayOfIntegers.Length; tmpCounter++)
+            {
+                //if the max value is less than the number in the array, set the max value to that number
+                if (tmpMaxValue < tmpArrayOfIntegers[tmpCounter])
+                {
+                    tmpMaxValue = tmpArrayOfIntegers[tmpCounter];
+                }
+            }
+
+            //return the max value
+            return tmpMaxValue;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //variables
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUM = 500;
+            int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
+            int counter, randomNum, maxValue;
+
+            maxValue = 0;
+            Random randomNumGenerator = new Random();
+
+            //clear the list box
+            this.lstNumbers.Items.Clear();
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                //get the random number
+                randomNum = randomNumGenerator.Next(1, MAX_RANDOM_NUM);
+
+                //assign the number to the array
+                arrayOfIntegers[counter] = randomNum;
+
+                //add the number to the list box
+                this.lstNumbers.Items.Add(randomNum);
+
+                //refresh the form
+                this.Refresh();
+            }
+
+            //get the max value from the array
+            maxValue = GetMaxValue(arrayOfIntegers);
+
+            //display the max value
+            this.lblAnswer.Text = "The max value is " + maxValue; 
+        }
+    }
+}
